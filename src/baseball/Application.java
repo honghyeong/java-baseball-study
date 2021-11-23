8cdb65289f7c1e5ffac27c2c4ec64a0a1913edb7
honghyeong <scarletgold0601@gmail.com>
2021-11-23 15:06:55 +0900

    create java-baseball-study

---
+++ src/baseball/Application.java
@@ -0,0 +1,12 @@
+package baseball;
+
+import java.util.Scanner;
+
+public class Application {
+    public static void main(String[] args){
+        final Scanner scanner = new Scanner(System.in);
+
+        String input= scanner.next();
+
+    }
+}
